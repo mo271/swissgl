c70cb3efa0f2b195125e1d31ed9b308a1853e258
Moritz Firsching <firsching@google.com>
2023-03-10 14:19:26 +0100

    limits

--- demo/main.js
+++ demo/main.js
@@ -70,7 +70,7 @@ class DemoApp {
                 if (lastTouchDistance !== null) {
                     let [yaw, pitch, dist] = this.viewParams.cameraYPD;
                     dist += (lastTouchDistance - touchDistance)*.05;
-                    dist = Math.min(Math.max(dist, 5), 30);
+                    dist = Math.min(Math.max(dist, 0.01), 20);
                     this.viewParams.cameraYPD.set([yaw, pitch, dist]);
                 }
                 lastTouchDistance = touchDistance;
